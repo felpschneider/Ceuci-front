246b83410f8a903f6bd98ed09723c693e9695c85
unknown <kamillasr15@gmail.com>
2022-08-10 11:09:23 -0300

    Feat Componente Categoria

---
+++ ceuci/src/components/categoria/listacategoria/ListaCategoria.tsx
@@ -0,0 +1,53 @@
+
+import React from 'react'
+import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Link } from 'react-router-dom'
+import './ListaCategoria.css';
+
+
+function ListaCategoria() {
+
+    return (
+        <>
+            <Box m={2} >
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Categoria
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                            Tipo
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Box display="flex" justifyContent="center" mb={1.5} >
+
+                            <Link to="" className="text-decorator-none">
+                                <Box mx={1}>
+                                    <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                                        Atualizar
+                                    </Button>
+                                </Box>
+                            </Link>
+                            <Link to="" className="text-decorator-none">
+                                <Box mx={1}>
+                                    <Button variant="contained" size='small' color="secondary">
+                                        Deletar
+                                    </Button>
+                                </Box>
+                            </Link>
+                        </Box>
+                    </CardActions>
+                </Card>
+            </Box>
+        </>
+    );
+}
+
+
+export default ListaCategoria;
+
+
+
+
+
